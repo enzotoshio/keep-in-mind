63820658ce94e60b3a3766016b7860e259cc88ed
Enzo Toshio <enzo.toshiba@gmail.com>
2013-11-24 02:52:09 -0200

    estrutura, estilos e script base

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var cardContainer = document.getElementById('card-container');
+
+    if (cardContainer.addEventListener) {
+        cardContainer.addEventListener('click', toggleClass, false);
+    }
+    else{
+        cardContainer.attachEvent('onclick', toggleClass);
+    }
+});
+
+function toggleClass () {
+    this.classList.toggle("flip");
+}
+
+//Caso detectemos incopatibilidade nos browser, trocar para este plugin: http://lab.smashup.it/flip/
