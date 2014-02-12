73bd72686a8b62257362beee75e4e79c9210fc06
leonardowolter <leocwolter@gmail.com>
2014-02-12 17:21:41 -0200

    melhorando nome dos metodos

--- framework/spec/paramPrettifierSpec.js
+++ framework/spec/paramPrettifierSpec.js
@@ -2,49 +2,49 @@ var prettifier = require('../paramPrettifier');
 
 describe("ParamPrettifier", function(){
 
-	it("should convert a property 'user.name' a object to data = {user : {name: 'leo'}}", function(done){
+	it("should convert one level property to object", function(done){
 		var body = {'user.name' : 'leo'};
 		var data = prettifier.prettify(body);
 		expect(data).toEqual({user : {name : 'leo'}});
 		done();
 	});
 
-	it("should convert two properties 'user.name'  and 'user.email' to a object data = {user : {name: 'enzo', email : 'enzo@enzo.com'}", function(done){
+	it("should convert two times one level property  to object", function(done){
 		var body = {'user.name' : 'enzo', 'user.email' : 'enzo@enzo.com'};
 		var data = prettifier.prettify(body);
 		expect(data).toEqual({user : {name : 'enzo', email :'enzo@enzo.com'}});
 		done();
 	});
 
-	it("data should be equal to body if there are no objects to be converted", function(done){
+	it("should be equal to body if there are no objects to be converted", function(done){
 		var body = {'name' : 'enzo'};
 		var data = prettifier.prettify(body);
 		expect(data).toEqual(body);
 		done();
 	});
 
-	it("should convert two levels of objects: 'deck.card.title' to data = {deck : {card : {title : 'somecard'}}}", function(done){
+	it("should convert two levels property to objects", function(done){
 		var body = {'deck.card.title' : 'somecard'};
 		var data = prettifier.prettify(body);
 		expect(data).toEqual({deck : {card : {title : 'somecard'}}});
 		done();
 	});
 
-	it("should convert two objects: 'card.title' and 'user.name'", function(done){
+	it("should convert two objects", function(done){
 		var body = {'card.title' : 'somecard', 'user.name' : 'leo'};
 		var data = prettifier.prettify(body);
 		expect(data).toEqual({card : {title : 'somecard'}, user : {name : 'leo'}});
 		done();
 	});
 
-	it("should convert two objects of different levels: 'card.title' and 'name'", function(done){
+	it("should convert two objects of different levels", function(done){
 		var body = {'card.title' : 'somecard', 'name' : 'leo'};
 		var data = prettifier.prettify(body);
 		expect(data).toEqual({card : {title : 'somecard'}, name : 'leo'});
 		done();
 	});
 
-	it("should convert two objects of different levels: 'deck.card.title' and 'user.name'", function(done){
+	it("should convert two objects of different levels", function(done){
 		var body = {'deck.card.title' : 'somecard', 'user.name' : 'leo'};
 		var data = prettifier.prettify(body);
 		expect(data).toEqual({deck : {card : {title : 'somecard'}}, user : {name : 'leo'}});
