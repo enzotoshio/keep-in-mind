d966fcd9be5a7c8ace47d4763d6fde736b3f1b16
leonardowolter <leocwolter@gmail.com>
2014-02-21 11:36:37 -0300

    só conecta se o mongo conectou

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ var http = require('http');
 var server = http.createServer(app);
 var io = require("socket.io").listen(server);
 var config = require('./config/config');
+var mongoose = require ("mongoose");
 
 
 require('./config/express')(app, config);
 require('./config/socketio')(io, config);
-require('./config/mongoose')(config);
 require('./framework/config')(app);
+bootstrap(config);
 
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+function bootstrap(config){
+	var uriString = config.db;
+	mongoose.connect(uriString, function (err, res) {
+		if (err) throw err;
+
+		server.listen(app.get('port'), function(){
+		  console.log('Express server listening on port ' + app.get('port'));
+		});
+
+	  	console.log ('Succeeded connected to: ' + uriString);
+	});
+}
 
 exports.io = io;
